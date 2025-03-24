15c5b9a17a31ba0045e9546da34bf783331a0a6b
Kani Maru <kani@kemomi.de>
2025-03-24 23:10:55 +0100

    Initial commit of the generator

---
+++ StringExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+namespace GenUtilities;
+
+public static class StringExtension {
+	public static string SnakeToCamelCase(this string str) {
+		return str.Split(["_"], StringSplitOptions.RemoveEmptyEntries)
+			.Select(s => char.ToUpperInvariant(s[0]) + s.Substring(1, s.Length - 1))
+			.Aggregate(string.Empty, (s1, s2) => s1 + s2);
+	}
+}
